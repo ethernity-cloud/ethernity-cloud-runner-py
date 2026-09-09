71efa4bdef4743fdc0e8ffbe7b4ba8897fa84fe0
Ethernity CLOUD <contact@ethernity.cloud>
2026-09-09 16:54:01 +0200

    runner: release 0.4.25

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ print(this_directory);
 
 setup(
     name="ethernity-cloud-runner-py",
-    version="0.4.24",
+    version="0.4.25",
     url="https://github.com/ethernity-cloud/ethernity-cloud-runner-py",
     author="Ethernity Cloud Team",
     author_email="contact@ethernity.cloud",
